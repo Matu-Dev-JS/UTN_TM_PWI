37ad05388e17fa68fe4ae57e6e651cc27294acc8
matu.dev <matu.a.gimenez2002@gmail.com>
2024-06-06 12:20:39 -0300

    clase 19

--- clase-16/registroProducto.js
+++ clase-16/registroProducto.js
@@ -19,3 +19,171 @@ Confirmar producto:
 Si el usuario elige cancelar (con el button) o escribe 'CANCELAR' o 'NO' se volvera a comenzar
 Si el usuario presiona aceptar o escribe 'SI' entonces la funcion creara un objeto del producto y lo mostrara por consola
 */
+const categoriasDisponibles = ['tecnologia', 'hogar', 'otros']
+
+const validarStringConMinimo = (valor, minimo) => valor && isNaN(valor) && valor.length >= minimo 
+const validarTitulo = (valor) => validarStringConMinimo(valor, 4)
+const validarPrecio = (valor) => valor && !isNaN(valor) && valor > 1
+const validarDescripcion = (valor) => validarStringConMinimo(valor, 10)
+const validarCategoria = (valor) => categoriasDisponibles.includes(valor)
+const validarMarca = (valor) => validarStringConMinimo(valor, 3)
+const validarNegacion = (confirmacion) => confirmacion === null || confirmacion.toLowerCase() === 'cancelar' || confirmacion.toLowerCase() === 'no'
+
+
+const crearObjetoConfeccion  = (mensaje, error, validacion) =>{
+    return {
+        mensaje: mensaje,
+        error: error, 
+        validacion: validacion
+    }
+}
+
+const DATOS_PRODUCTO = {
+    TITULO: crearObjetoConfeccion(
+        'Ingrese un titulo min (4) caracteres:',
+        'Error al ingresar titulo, ingrese nuevamente:',
+        validarTitulo
+    ),
+    PRECIO: crearObjetoConfeccion(
+        'Ingrese un precio positivo mayor a $1',
+        'Error al ingresar precio, ingrese nuevamente',
+        validarPrecio
+    ),
+    DESCRIPCION: crearObjetoConfeccion(
+        'Ingrese una descripcion min (10) caracteres',
+        'Error al ingresar descripcion, ingrese nuevamente',
+        validarDescripcion
+    ),
+    CATEGORIA:crearObjetoConfeccion(
+        'Ingrese una caregoria valida (categorias validas: ' + categoriasDisponibles.join(', ') +')',
+        'Error al ingresar una categoria, (categorias validas: ' + categoriasDisponibles.join(', ') +')', 
+        validarCategoria
+    ),
+    MARCA: crearObjetoConfeccion(
+        'Ingrese una marca min (3) caracteres',
+        'Error al ingresar marca, ingrese nuevamente',
+        validarMarca
+    ),
+} 
+
+const solicitarDato = (objetoDeConfeccion) => {
+
+    let dato = prompt(objetoDeConfeccion.mensaje)
+    while(!objetoDeConfeccion.validacion(dato)){
+        dato = prompt(objetoDeConfeccion.error)
+    }
+    return dato
+}
+
+
+const registrarProducto = () => {
+
+
+    let titulo = solicitarDato(DATOS_PRODUCTO.TITULO)
+    let precio = solicitarDato(DATOS_PRODUCTO.PRECIO)
+    let descripcion = solicitarDato(DATOS_PRODUCTO.DESCRIPCION)
+    let categoria = solicitarDato(DATOS_PRODUCTO.CATEGORIA)
+    let marca = solicitarDato(DATOS_PRODUCTO.MARCA)
+
+    let mensajeConfirmacion = `
+    Esta seguro de confirmar el producto:
+    Titulo: ${titulo}
+    Precio: ${precio}
+    Descripcion: ${descripcion}
+    Categoria: ${categoria}
+    Marca: ${marca}
+    `
+    let confirmacion = prompt(mensajeConfirmacion)
+    if(validarNegacion(confirmacion)){
+        registrarProducto()
+    }
+    else{
+        const producto = {
+            titulo,
+            precio,
+            descripcion,
+            categoria,
+            marca
+        }
+        console.log(producto)
+    }
+}
+
+
+registrarProducto()
+
+
+
+
+
+/* 
+
+const registrarProducto = () => {
+    
+    const producto = {}
+    for (let propiedad in DATOS_PRODUCTO){
+        let dato = solicitarDato(DATOS_PRODUCTO[propiedad])
+        producto[propiedad.toLowerCase()] = dato
+    }
+
+
+
+    let mensajeConfirmacion = `
+    Esta seguro de confirmar el producto:
+    Titulo: ${producto.titulo}
+    Precio: ${producto.precio}
+    Descripcion: ${producto.descripcion}
+    Categoria: ${producto.categoria}
+    Marca: ${producto.marca}
+    `
+    let confirmacion = prompt(mensajeConfirmacion)
+    if(validarNegacion(confirmacion)){
+        registrarProducto()
+    }
+    else{
+        console.log(producto)
+    }
+}
+
+
+registrarProducto() */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let confirmar = prompt('Confirmar {plantilla}')
+
+// if(confirmar == null || confirmar == 'NO' || confirmar == 'CANCELAR' ){
+//     /* Vamos a volver a solicitar */
+// }
+// /* Caso contrario creamos el objeto */
+
+/* 
+let dato = prompt("ingrese un dato")
+while(!dato){
+    dato = prompt("Dato mal ingresado, ingrese nuevamente")
+} */
+
+/*     while(!(categoria == 'hogar' || categoria == 'tecnolgia')) */
+
+
+/* dato = isNaN  Es imposible que funcione si no le paso un parametro a la funcion, el = es REASIGNACION y la funcion isNaN se debe invocar*/
+
+/* isNaN(precio) //Me retorna el boolean si precio es numero retorna false sino retorna true */
