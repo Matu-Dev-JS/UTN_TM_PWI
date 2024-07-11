d4764e8cfeadb0667f4985b7f75f5adbf70b4719
matu.dev <matu.a.gimenez2002@gmail.com>
2024-07-11 09:40:16 -0300

    clase 24

---
+++ react-proyecto-clase-24/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ColorCardList } from './Components'
+import { colorCardList } from './data'
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <ColorCardList colorCardList={colorCardList}/>
+    </>
+  )
+}
+
+export default App
---
+++ react-proyecto-clase-24/src/Components/index.js
@@ -0,0 +1,5 @@
+import ColorCard from "./ColorCard/ColorCard";
+import ColorCardList from "./ColorCardList/ColorCardList";
+import ColorItemList from "./ColorItemList/ColorItemList";
+
+export {ColorCard, ColorCardList, ColorItemList}
